0344d0d212cb1c1ebea4bb7514162d7826c4dd52
Marc Veens <marcveens@hotmail.com>
2024-12-07 13:53:34 +0100

    Day 6 part 2

--- scripts/day06/main.py
+++ scripts/day06/main.py
@@ -1,3 +1,3 @@
 from path_walker import path_walker
 
-print(f"Part 1: {path_walker("input.txt")}")
+print(f"Part 2: {path_walker("input.txt")}")
--- scripts/day06/path_walker.py
+++ scripts/day06/path_walker.py
@@ -1,18 +1,42 @@
 from get_file import get_file_contents
 
 def path_walker(file_contents):
+  infinites = 0
+  
   walker = Walker()
   
   walker.set_map(file_contents)
   
-  walker.go_walk()
-    
-  positions_visited = len(walker.get_positions_visited())
+  try:
+    walker.go_walk()
+  except Exception as e:
+    print(e)
   
-  return positions_visited
+  print(f"Total: {len(walker.positions_visited)}")
+  
+  for idx, point in enumerate(walker.positions_visited):
+    walker.walking_direction = "UP"
+    walker.positions_visited = []
+    walker.set_map(file_contents)
+    
+    if not point.get("is_guard"):
+      walker.set_extra_obstruction(point)
+  
+      try:
+        walker.go_walk()
+        print(f"id {idx}: succeeded")
+      except Exception as e:
+        print(f"id {idx}: {e}")
+        infinites += 1
+        
+  # positions_visited = len(walker.get_positions_visited())
+  
+  # return positions_visited
+  
+  return infinites
   
 class Walker:
-  map = []
+  map = {}
   walking_direction = "UP"
   positions_visited = []
   
@@ -24,38 +48,34 @@ class Walker:
     
     while self.get_next_position(current_position):
       current_position = self.get_next_position(current_position)
-      print(f"next {current_position}")
+      # print(f"next {current_position}")
       
       if self.is_unique_position(current_position):
         self.positions_visited.append({
-          "row": current_position.get("row"),
-          "col": current_position.get("col")
+          "row": current_position["row"],
+          "col": current_position["col"],
+          "is_guard": current_position["is_guard"],
+          "direction": self.walking_direction
         })
+      elif self.is_starting_position_and_direction(current_position):
+        raise Exception("infinite!")
   
   def get_next_position(self, current_position):
     if self.walking_direction == "UP":
-      next_position = next((x for x in self.map if 
-                            x.get("row") == current_position.get("row") - 1 and
-                            x.get("col") == current_position.get("col")), None)
+      next_position = self.map.get(f"{current_position["row"] - 1}-{current_position["col"]}", None)
     elif self.walking_direction == "RIGHT":
-      next_position = next((x for x in self.map if 
-                            x.get("row") == current_position.get("row") and
-                            x.get("col") == current_position.get("col") + 1), None)
+      next_position = self.map.get(f"{current_position["row"]}-{current_position["col"] + 1}", None)
     elif self.walking_direction == "DOWN":
-      next_position = next((x for x in self.map if 
-                            x.get("row") == current_position.get("row") + 1 and
-                            x.get("col") == current_position.get("col")), None)
+      next_position = self.map.get(f"{current_position["row"] + 1}-{current_position["col"]}", None)
     elif self.walking_direction == "LEFT":
-      next_position = next((x for x in self.map if 
-                            x.get("row") == current_position.get("row") and
-                            x.get("col") == current_position.get("col") - 1), None)
+      next_position = self.map.get(f"{current_position["row"]}-{current_position["col"] - 1}", None)
     
     # If no next_position, it's complete
     if not next_position:
       return None   
     
     # If next_position is wall: rotate
-    if next_position and next_position.get("is_wall"):
+    if next_position and next_position["is_wall"]:
       if self.walking_direction == "UP":
         self.walking_direction = "RIGHT"
       elif self.walking_direction == "RIGHT":
@@ -72,17 +92,27 @@ class Walker:
     
   def is_unique_position(self, position):
     return not next((x for x in self.positions_visited if 
-                     x.get("row") == position.get("row") and
-                     x.get("col") == position.get("col")), None)
-
+                    x.get("row") == position.get("row") and
+                    x.get("col") == position.get("col")), None)
+    
+  def is_starting_position_and_direction(self, position):
+    return next((x for x in self.positions_visited if 
+                x.get("row") == position.get("row") and
+                x.get("col") == position.get("col") and
+                x.get("direction") == self.walking_direction), None)
+  
   def get_guard_position(self):
-    return next((x for x in self.map if x.get("is_guard") == True), None)
-  
-  def get_positions_visited(self):
-    return self.positions_visited
-  
+    for key in self.map:
+      if self.map[key]["is_guard"]:
+        return self.map[key]    
+    
+    return None
+        
+  def set_extra_obstruction(self, position):
+    self.map[f"{position["row"]}-{position["col"]}"]["is_wall"] = True
+    
   def create_map(self, file_contents):
-    matrix = []
+    matrix = {}
     
     split_rows = file_contents.split("\n")
     
@@ -90,18 +120,20 @@ class Walker:
       points = list(row)
       
       for col_idx, point in enumerate(points):
-        matrix.append({
+        matrix[f"{row_idx}-{col_idx}"] = {
           "row": row_idx,
           "col": col_idx,
           "is_wall": point == "#",
           "is_floor": point != "#",
           "is_guard": point == "^"
-        })
+        }
         
         if point == "^":
           self.positions_visited.append({
             "row": row_idx,
-            "col": col_idx
+            "col": col_idx,
+            "is_guard": True,
+            "direction": self.walking_direction
           })
-        
+          
     return matrix
